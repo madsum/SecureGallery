87a6ea57fd87968355ca0c1eadef894c5dc50de8
Masum Islam <masum.islam@metropolia.fi>
2013-03-29 11:44:37 +0200

    New src file BaseDialog. It will be inherted later.

---
+++ src/fi/masum/securegallery/BaseDialog.java
@@ -0,0 +1,75 @@
+package fi.masum.securegallery;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public abstract class BaseDialog implements DialogInterface.OnClickListener
+{
+	public interface OnDismissListener
+	{
+		public void onDialogDismissed(BaseDialog dialog);
+	}
+
+	protected AlertDialog.Builder m_Builder;
+	protected Context m_Context;
+	protected OnDismissListener m_DismissListener; 
+	
+	public int RequestCode = 0;
+	public String TitleText;
+	public String BodyText;
+	public String AcceptText = "Ok";
+	public String CancelText = "Cancel";
+	public boolean DidAccept = false;
+	public boolean choiceDialog = false;
+	public boolean yesNoDialog = false;
+
+
+	protected BaseDialog(Context context,  OnDismissListener dismissListener, String title, String bodyText)
+	{
+		this.TitleText = title;
+		this.BodyText = bodyText;
+		this.m_Builder = new AlertDialog.Builder(context);
+		this.m_Context = context;
+		this.m_DismissListener = dismissListener;
+	}
+
+	protected void prepareDialog()
+	{
+		this.m_Builder.setTitle(this.TitleText);
+		this.m_Builder.setPositiveButton(AcceptText, this);
+		this.m_Builder.setNegativeButton(CancelText, this);
+	}
+
+	public void show()
+	{
+		this.prepareDialog();
+		AlertDialog dialog = this.m_Builder.create();
+		this.onCreate(dialog);
+		dialog.show();
+	}
+
+	protected void onCreate(AlertDialog dialog)
+	{
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int buttonId)
+	{
+		switch(buttonId)
+		{
+			case DialogInterface.BUTTON_POSITIVE:
+				this.DidAccept = true;
+			case DialogInterface.BUTTON_NEGATIVE:
+				{
+					dialog.dismiss();
+					if (this.m_DismissListener != null)
+					{
+						this.m_DismissListener.onDialogDismissed(this);
+					}
+				} break;
+			default:
+				break;
+		}
+	}
+}
